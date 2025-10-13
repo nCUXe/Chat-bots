e288af9197dd062833951df284e17dbba8b0430f
Egor Bessonov <egorkabessonov1@gmail.com>
2025-10-13 09:28:45 +0300

    (fix/feat/update): added a message handler

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,11 +10,14 @@ def main() -> None:
             updates = bot.telergam_client.getUpdates(next_update_offset)
             bot.database_client.persist_updates(updates)
             for update in updates:
-                bot.telergam_client.sendMessage(
-                    chat_id=update["message"]["chat"]["id"],
-                    text=update["message"]["text"],
-                )
-                print(".", end="", flush=True)
+                if "message" in update and "text" in update["message"]:
+                    bot.telergam_client.sendMessage(
+                        chat_id=update["message"]["chat"]["id"],
+                        text=update["message"]["text"],
+                    )
+                    print(".", end="", flush=True)
+                else:
+                    print("ONLY TEXT MESSAGE!")
                 next_update_offset = max(next_update_offset, update["update_id"] + 1)
             time.sleep(1)
     except KeyboardInterrupt:
