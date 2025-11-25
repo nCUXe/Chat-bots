9b710b3e4053e41622f70dfbc13aa573603423dc
Egor Bessonov <egorkabessonov1@gmail.com>
2025-11-25 19:28:05 +0300

    add docker and sqlite replaced by postgres

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,20 +1,17 @@
-import bot.long_polling
 from bot.dispatcher import Dispatcher
-from bot.domain.messenger import Messenger
-from bot.domain.storage import Storage
-from bot.handlers import get_handlers
+from bot import get_handlers
+from bot.long_polling import start_long_polling
 from bot.infrastructure.messenger_telegram import MessengerTelegram
-from bot.infrastructure.storage_sqlite import StorageSqlite
+from bot.infrastructure.storage_postgres import StoragePostgres
 
 
 def main() -> None:
     try:
-        storage: Storage = StorageSqlite()
-        messenger: Messenger = MessengerTelegram()
-
+        storage = StoragePostgres()
+        messenger = MessengerTelegram()
         dispatcher = Dispatcher(storage, messenger)
-        dispatcher.add_handlers(*get_handlers())
-        bot.long_polling.start_long_polling(dispatcher, messenger)
+        dispatcher.add_handler(*get_handlers())
+        start_long_polling(dispatcher, messenger)
     except KeyboardInterrupt:
         print("\nBye!")
 
